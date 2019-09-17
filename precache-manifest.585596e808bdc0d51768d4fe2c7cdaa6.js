057a5649ac32f3243988392cfd7f5932243c6ee6
ChristophSiegenthaler <christoph.siegenthaler@gmail.com>
2019-09-17 21:03:35 +0200

    Updates

---
+++ precache-manifest.585596e808bdc0d51768d4fe2c7cdaa6.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a94f1cb9be042fdc5300d9660b499c",
+    "url": "https://uniswap.ch/index.html"
+  },
+  {
+    "revision": "3b81605bbc2c8e2dbfb3",
+    "url": "https://uniswap.ch/static/css/0.07de6c03.chunk.css"
+  },
+  {
+    "revision": "bf068a0274420fb29c63",
+    "url": "https://uniswap.ch/static/css/4.d75e0ccb.chunk.css"
+  },
+  {
+    "revision": "3b81605bbc2c8e2dbfb3",
+    "url": "https://uniswap.ch/static/js/0.e6ba369c.chunk.js"
+  },
+  {
+    "revision": "8712098d4424bf4556c7",
+    "url": "https://uniswap.ch/static/js/1.9ca017b9.chunk.js"
+  },
+  {
+    "revision": "eb262596a46c25d70c58",
+    "url": "https://uniswap.ch/static/js/10.b437285b.chunk.js"
+  },
+  {
+    "revision": "bf068a0274420fb29c63",
+    "url": "https://uniswap.ch/static/js/4.4b6b6fa5.chunk.js"
+  },
+  {
+    "revision": "acd33fc698a25db5a71e",
+    "url": "https://uniswap.ch/static/js/5.b4e095bd.chunk.js"
+  },
+  {
+    "revision": "326d605ec2fcc53fcaa3",
+    "url": "https://uniswap.ch/static/js/6.1df3b272.chunk.js"
+  },
+  {
+    "revision": "09bda54d34a234056057",
+    "url": "https://uniswap.ch/static/js/7.fdd20747.chunk.js"
+  },
+  {
+    "revision": "0a5db0c01749e3651a34",
+    "url": "https://uniswap.ch/static/js/8.3b5eebcf.chunk.js"
+  },
+  {
+    "revision": "e0aae02bc063f7f70db2",
+    "url": "https://uniswap.ch/static/js/9.c634a82e.chunk.js"
+  },
+  {
+    "revision": "9d61bf5bb842b4afb20c",
+    "url": "https://uniswap.ch/static/js/main.1614f2be.chunk.js"
+  },
+  {
+    "revision": "372168704807412f11c5",
+    "url": "https://uniswap.ch/static/js/runtime~main.60ce4108.js"
+  },
+  {
+    "revision": "ed2a1dad16cb9a4b9afd788ddaae7290",
+    "url": "https://uniswap.ch/static/media/circle-grey.ed2a1dad.svg"
+  },
+  {
+    "revision": "2d975615c4c409c3b6b00e8ae7c5767a",
+    "url": "https://uniswap.ch/static/media/circle.2d975615.svg"
+  },
+  {
+    "revision": "b20914ec5482543a0b1b2c6d5509ab96",
+    "url": "https://uniswap.ch/static/media/dropdown-blue.b20914ec.svg"
+  },
+  {
+    "revision": "7d32d2fa19d17d6ab9f0e0067bebaf96",
+    "url": "https://uniswap.ch/static/media/dropdown.7d32d2fa.svg"
+  },
+  {
+    "revision": "b96d70e10dd30a64a0d122603577c8ae",
+    "url": "https://uniswap.ch/static/media/dropup-blue.b96d70e1.svg"
+  },
+  {
+    "revision": "802c6eacbb13c83b27c39be505228231",
+    "url": "https://uniswap.ch/static/media/ethereum-logo.802c6eac.svg"
+  },
+  {
+    "revision": "674400972753804891ec372652944539",
+    "url": "https://uniswap.ch/static/media/magnifying-glass.67440097.svg"
+  },
+  {
+    "revision": "e8021e51723d455b2f9fa2446808d2a3",
+    "url": "https://uniswap.ch/static/media/plus-blue.e8021e51.svg"
+  },
+  {
+    "revision": "cc0a245174670956764c52ccc9238c08",
+    "url": "https://uniswap.ch/static/media/question.cc0a2451.svg"
+  },
+  {
+    "revision": "b5a91f02ad9b04f065d0d4683428bb0f",
+    "url": "https://uniswap.ch/static/media/unicorn.b5a91f02.svg"
+  },
+  {
+    "revision": "b0200bf7cbbb26c32989e7a0e4d19eef",
+    "url": "https://uniswap.ch/static/media/x.b0200bf7.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9d420553b09d419a73da96b405a91e9f.js"
+  "https://uniswap.ch/precache-manifest.585596e808bdc0d51768d4fe2c7cdaa6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://uniswap.ch/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
