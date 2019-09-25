9337a9bbe7a8a4f79fc5dbd35f88aef4dfa0547e
ChristophSiegenthaler <christoph.siegenthaler@gmail.com>
2019-09-25 14:11:34 +0200

    cleanup i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,5 @@
-import i18next from 'i18next'	import i18next from 'i18next'
-import { initReactI18next } from 'react-i18next'	import { initReactI18next } from 'react-i18next'
+import i18next from 'i18next'
+import { initReactI18next } from 'react-i18next'
 import XHR from 'i18next-xhr-backend'	
 import LanguageDetector from 'i18next-browser-languagedetector'	
 
